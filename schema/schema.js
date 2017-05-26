a560f830812fabb4d95acceda5a43b50a3b89a13
Eric Buchmann <me@ebuchmann.com>
2017-05-25 17:49:55 -0700

    multiple entry points

--- schema/schema.js
+++ schema/schema.js
@@ -7,12 +7,28 @@ const {
   GraphQLSchema,
 } = graphql;
 
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+  },
+});
+
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      async resolve(parentValue, args) {
+        const { data } = await axios.get(`http://localhost:3000/companies/${parentValue.companyId}`);
+        return data;
+      },
+    },
   },
 });
 
@@ -24,6 +40,14 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLString } },
       async resolve(parentValue, args) {
         const { data } = await axios.get(`http://localhost:3000/users/${args.id}`);
+        return data;
+      },
+    },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      async resolve(parentValue, args) {
+        const { data } = await axios.get(`http://localhost:3000/companies/${args.id}`);
         return data;
       },
     },
